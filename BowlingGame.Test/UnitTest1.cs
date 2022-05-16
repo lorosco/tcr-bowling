6b610746d0ae4c922018907e97a128fb9bfe9e86
lorosco <leonard.orosco2.0@gmail.com>
2022-05-16 16:54:07 +0200

    Adding prettier

--- BowlingGame.Test/UnitTest1.cs
+++ BowlingGame.Test/UnitTest1.cs
@@ -6,33 +6,37 @@ namespace BowlingGame.Test
     {
         [TestMethod]
         // initialisation de la partie avec un score à 0
-        public void initScoreIsZero(){
+        public void initScoreIsZero()
+        {
             var game = new Partie();
             Assert.AreEqual(0, game.score);
         }
 
         [TestMethod]
-        public void initSquareWith10Keel(){
+        public void initSquareWith10Keel()
+        {
             var game = new Partie();
-            Assert.AreEqual(10,game.currentKeel);
+            Assert.AreEqual(10, game.currentKeel);
 
             game.dropKeel(5);
 
             game.nextSquare();
-            Assert.AreEqual(10,game.currentKeel);
+            Assert.AreEqual(10, game.currentKeel);
         }
 
         [TestMethod]
-        public void dropKeelUpdateScore(){
+        public void dropKeelUpdateScore()
+        {
             // lorsque le joueur effectue un lancer et fait tomber des quilles, le score est mis à jour
             var game = new Partie();
             game.dropKeel(3);
 
-            Assert.AreEqual(game.score,3);
+            Assert.AreEqual(game.score, 3);
         }
 
         [TestMethod]
-        public void isSquareResetWhenAllKeelsDown(){
+        public void isSquareResetWhenAllKeelsDown()
+        {
             Partie game = new Partie();
             game.dropKeel(10);
 
@@ -40,7 +44,8 @@ namespace BowlingGame.Test
         }
 
         [TestMethod]
-        public void isSquareResetWhenAllLaunchDone(){
+        public void isSquareResetWhenAllLaunchDone()
+        {
             Partie game = new Partie();
             game.dropKeel(2);
 
@@ -50,9 +55,10 @@ namespace BowlingGame.Test
         }
 
         [TestMethod]
-        public void isSpareCounted(){
+        public void isSpareCounted()
+        {
             Partie partie = new Partie();
-            
+
             partie.dropKeel(7);
             partie.dropKeel(3);
 
@@ -64,7 +70,8 @@ namespace BowlingGame.Test
         }
 
         [TestMethod]
-        public void isStrikeCounted(){
+        public void isStrikeCounted()
+        {
             Partie partie = new Partie();
 
             partie.dropKeel(10);
@@ -73,13 +80,13 @@ namespace BowlingGame.Test
 
             partie.dropKeel(10);
 
-            Assert.AreEqual(partie.score,30);
+            Assert.AreEqual(partie.score, 30);
 
             Assert.AreEqual(partie.isN2LaunchStrike && partie.isN1LaunchStrike, true);
 
             partie.dropKeel(3);
 
-            Assert.AreEqual(partie.score,39);
+            Assert.AreEqual(partie.score, 39);
 
             partie.dropKeel(3);
 
@@ -89,36 +96,38 @@ namespace BowlingGame.Test
         }
 
         [TestMethod]
-        public void isLastSquareCanBe3ShootOr2(){
+        public void isLastSquareCanBe3ShootOr2()
+        {
             Partie partieStrike = new Partie();
             for (int i = 0; i < 9; i++)
             {
                 partieStrike.dropKeel(5);
                 partieStrike.dropKeel(3);
-                Assert.AreEqual(partieStrike.score,8*(i+1));
+                Assert.AreEqual(partieStrike.score, 8 * (i + 1));
             }
             partieStrike.dropKeel(10);
-            Assert.AreEqual(partieStrike.score,82);
+            Assert.AreEqual(partieStrike.score, 82);
             Assert.AreEqual(partieStrike.currentSquare, 10);
             partieStrike.dropKeel(10);
-            Assert.AreEqual(partieStrike.score,92);
+            Assert.AreEqual(partieStrike.score, 92);
             partieStrike.dropKeel(10);
             //le score final est affiché dans la console, une fois celui-ci affiché, la partie se réinitialise
             //Assert.AreEqual(partieStrike.score,102);
         }
 
         [TestMethod]
-        public void isLastSquareCanBe3ShootWithSpare(){
+        public void isLastSquareCanBe3ShootWithSpare()
+        {
             Partie partieSpare = new Partie();
             for (int i = 0; i < 9; i++)
             {
                 partieSpare.dropKeel(5);
                 partieSpare.dropKeel(3);
-                Assert.AreEqual(partieSpare.score,8*(i+1));
+                Assert.AreEqual(partieSpare.score, 8 * (i + 1));
             }
             partieSpare.dropKeel(5);
             partieSpare.dropKeel(5);
-            Assert.AreEqual(partieSpare.score,82);
+            Assert.AreEqual(partieSpare.score, 82);
             Assert.AreEqual(partieSpare.currentSquare, 10);
             partieSpare.dropKeel(5);
             //le score final est affiché dans la console, une fois celui-ci affiché, la partie se réinitialise
@@ -126,16 +135,17 @@ namespace BowlingGame.Test
         }
 
         [TestMethod]
-        public void isLastSquareCanBe2Shoot(){
+        public void isLastSquareCanBe2Shoot()
+        {
             Partie partieSpare = new Partie();
             for (int i = 0; i < 9; i++)
             {
                 partieSpare.dropKeel(5);
                 partieSpare.dropKeel(3);
-                Assert.AreEqual(partieSpare.score,8*(i+1));
+                Assert.AreEqual(partieSpare.score, 8 * (i + 1));
             }
             partieSpare.dropKeel(5);
-            Assert.AreEqual(partieSpare.score,77);
+            Assert.AreEqual(partieSpare.score, 77);
             partieSpare.dropKeel(2);
             //le score final est affiché dans la console, une fois celui-ci affiché, la partie se réinitialise
             //Assert.AreEqual(partieSpare.score,79);
--- BowlingGame/Partie.cs
+++ BowlingGame/Partie.cs
@@ -1,10 +1,11 @@
 namespace BowlingGame
 {
-    public class Partie{
-        public int score {get; private set;}
+    public class Partie
+    {
+        public int score { get; private set; }
 
-        public int currentSquare {get; private set;} = 1;
-        public int currentKeel {get; private set;} = 10;
+        public int currentSquare { get; private set; } = 1;
+        public int currentKeel { get; private set; } = 10;
         private int currentLaunch;
         public Boolean isNextLaunchSpare;
 
@@ -13,51 +14,63 @@ namespace BowlingGame
         //lancé n-2
         public Boolean isN2LaunchStrike = false;
 
-        public void dropKeel(int number){
-            if(isNextLaunchSpare){
+        public void dropKeel(int number)
+        {
+            if (isNextLaunchSpare)
+            {
                 this.score += number;
             }
-            if(isN1LaunchStrike){
+            if (isN1LaunchStrike)
+            {
                 this.score += number;
             }
-            if(isN2LaunchStrike){
+            if (isN2LaunchStrike)
+            {
                 this.score += number;
             }
             this.currentKeel -= number;
             currentLaunch++;
             //strike sur le dernier carreau
-            if(!(currentSquare == 10 && currentLaunch>1)){
+            if (!(currentSquare == 10 && currentLaunch > 1))
+            {
                 this.score += number;
             }
-            if(currentSquare == 10 && !isN1LaunchStrike && currentLaunch>1 && !isNextLaunchSpare && !isN2LaunchStrike){
+            if (currentSquare == 10 && !isN1LaunchStrike && currentLaunch > 1 && !isNextLaunchSpare && !isN2LaunchStrike)
+            {
                 this.score += number;
             }
             //Check all bonus incomming
             this.isN2LaunchStrike = isN1LaunchStrike;
             this.isN1LaunchStrike = currentLaunch == 1 && areAllKeelsDown();
             this.isNextLaunchSpare = currentLaunch == 2 && areAllKeelsDown();
-            if((isAllLaunchDone() || areAllKeelsDown()) && currentSquare!=10){
+            if ((isAllLaunchDone() || areAllKeelsDown()) && currentSquare != 10)
+            {
                 nextSquare();
             }
-            if(currentSquare == 10 && currentLaunch==2 && currentKeel>0 && !isN1LaunchStrike){
+            if (currentSquare == 10 && currentLaunch == 2 && currentKeel > 0 && !isN1LaunchStrike)
+            {
                 int finalScore = endGame();
-                Console.Write("Your final score is:" + finalScore);
+                Console.Write("Your final score is: " + finalScore);
             }
-            if(currentSquare == 10 && currentLaunch == 3){
+            if (currentSquare == 10 && currentLaunch == 3)
+            {
                 int finalScore = endGame();
                 Console.Write("Your final score is:" + finalScore);
             }
         }
 
-        public void nextSquare(){
+        public void nextSquare()
+        {
             this.currentKeel = 10;
-            if(this.currentSquare != 10){
+            if (this.currentSquare != 10)
+            {
                 this.currentLaunch = 0;
-                this.currentSquare +=1;
+                this.currentSquare += 1;
             }
         }
 
-        private int endGame(){
+        private int endGame()
+        {
             int score = this.score;
             this.currentLaunch = 0;
             this.currentSquare = 1;
@@ -67,11 +80,13 @@ namespace BowlingGame
             this.score = 0;
             return score;
         }
-        private Boolean areAllKeelsDown(){
+        private Boolean areAllKeelsDown()
+        {
             return currentKeel == 0;
         }
 
-        private Boolean isAllLaunchDone(){
+        private Boolean isAllLaunchDone()
+        {
             return currentLaunch == 2;
         }
 

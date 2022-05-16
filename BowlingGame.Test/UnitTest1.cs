9b6f882770b53becd58a199dbe87af5f1fdb418e
lorosco <leonard.orosco2.0@gmail.com>
2022-05-16 11:28:26 +0200

    Test1

--- BowlingGame.Test/UnitTest1.cs
+++ BowlingGame.Test/UnitTest1.cs
@@ -8,6 +8,20 @@ namespace BowlingGame.Test
         [TestMethod]
         public void TestMethod1()
         {
+            // initialisation de la partie avec un score à 0
+            public void intiScoreIsZero(){
+                var game = new Partie();
+                Assert.AreEqual(0, game.score());
+            }
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // initialisation de la partie avec un score à 0
+            public void intiScoreIsZero(){
+                var game = new Partie();
+                Assert.AreEqual(0, game.score());
+            }
         }
     }
 }
